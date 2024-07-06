00148932d185d48c8919b44b819447dbef2077e8
Levoslav <kubo.hajko@gmail.com>
2024-07-06 14:46:22 +0200

    lsc dataset added

--- src/experiments/compute_cumulation.py
+++ src/experiments/compute_cumulation.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Encode images using a specified model.")
-    parser.add_argument("dataset", type=str, help="Dataset name", choices=["marine", "photos"])
+    parser.add_argument("dataset", type=str, help="Dataset name", choices=["marine", "photos", "lsc"])
     parser.add_argument("labels", type=str, help="Type of labels", choices=["short", "long"])
     parser.add_argument("model", type=str, help="Model name", choices=["clip", "align", "blip2", "openclip"])
     parser.add_argument("version", type=str, help="Version name (only for 'openclip' model)", default=None)
